01780a14a5741581fb7c6e79cc6b48de7565aefa
Salomon Uwimana Masasu <180163847+masasu-salomon@users.noreply.github.com>
2026-09-10 20:52:25 +0200

    Add MyComponent with heading and paragraph

---
+++ my-first-react-app/src/MyComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello from MyComponent!</h1>
+      <p>This is a paragraph of text within my component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
